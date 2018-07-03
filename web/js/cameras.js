17bb596fb57ac787e146e8d1fa41c7166dcf7ffc
RobinTersou <tersou.robin@gmail.com>
2018-07-03 17:30:17 +0200

    c

--- web/js/cameras.js
+++ web/js/cameras.js
@@ -1,8 +1,14 @@
+import * as streamedian from 'Medias/streamedian/player.js';
+
 var app = app || {};
 
 app.cameras = {
     init : () => {
         console.log("Appel Script : cameras.js");
+    },
+
+    play : ( playerDiv ) => {
+        let player = new streamedian.WSPlayer(playerDiv, )
     }
 }
 
